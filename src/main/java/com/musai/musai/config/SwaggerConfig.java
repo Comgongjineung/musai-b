21f1fdbcfeb915381f8d41849c3a5c4e76022013
mhyeon-kim <lovelypsent@gmail.com>
2025-05-13 18:37:08 +0900

    ✨Feat: Add 스웨거 연동
    스웨거 연동 및 mongoDB 연결 설정 Resolves #3

---
+++ src/main/java/com/musai/musai/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.musai.musai.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    //http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Musai RestAPI Document")
+                .description("2025 한이음 Musai 프로젝트 API 명세서입니다.")
+                .version("1.0");
+    }
+}
---
+++ src/main/java/com/musai/musai/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.musai.musai.repository;
+
+import com.musai.musai.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    // 필요한 쿼리 메서드 추가 가능
+    User save(User user);
+
+    User findByNickname(String nickname);
+}
