de2cb8bcd37e2cca775c6d1f034df36a6d224121
mhyeon-kim <0214kkkk@naver.com>
2025-05-29 21:44:35 +0900

    ✨Feat: Update TTS 연결 수정
    TTS 연결 수정 Resolves #11

---
+++ src/main/java/com/musai/musai/dto/TtsRequestDTO.java
@@ -0,0 +1,14 @@
+package com.musai.musai.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description = "TTS 요청 DTO")
+public class TtsRequestDTO {
+    
+    @Schema(description = "음성으로 변환할 텍스트", example = "이 작품은 레오나르도 다 빈치의 모나리자입니다.")
+    private String text;
+} 
