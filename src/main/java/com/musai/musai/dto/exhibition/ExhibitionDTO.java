7324c78c58194ec9bab2681d6d6a4ccb5b3e120b
RYU <rjw5256@gmail.com>
2025-07-21 16:16:04 +0900

    ♻️  Feat: Update exhibition 정보 수정
    exhibition dto, entity 수정했습니다.
    
    Related to: #24

--- src/main/java/com/musai/musai/dto/exhibition/ExhibitionDTO.java
+++ src/main/java/com/musai/musai/dto/exhibition/ExhibitionDTO.java
@@ -1,25 +1,18 @@
 package com.musai.musai.dto.exhibition;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 public class ExhibitionDTO {
-    private String title; //제목
-    private String cntc_instt_nm; //연계기관명
-    private String description; //설명
-    private String image_object; //이미지 주소 (전시회 이미지)
-    private String genre; //전시회 장르
-    private String url; //홈페이지 주소
-    private String duration; //관람 시간
-    private String period; //전시회 기간
-    private String event_period; //전시회 시간
-    private String table_of_contents; //안내 및 유의사항
-    private String event_site; //장소
-    private String contributor; //주최
+    private Long exhi_id;      // 전시회 ID
+    private String title;      // 전시 이름
+    private String startDate;  // 시작 날짜
+    private String endDate;    // 종료 날짜
+    private String place;      // 장소
+    private String realmName;  // 전시 장르
+    private String thumbnail;  // 썸네일 이미지 주소
+    private String gpsX;       // 위도
+    private String gpsY;       // 경도
 }
