1c7d3cc82b96d6c057bb946c181115b9f89d0d5e
mhyeon-kim <0214kkkk@naver.com>
2025-07-24 17:58:32 +0900

    ♻️Ref: Update 북마크 API 응답값 수정
    북마크 추가 및 삭제 API 응답값 수정 Resolves #31

--- src/main/java/com/musai/musai/repository/bookmark/BookmarkRepository.java
+++ src/main/java/com/musai/musai/repository/bookmark/BookmarkRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     List<Bookmark> findByUserId(Long userId);
     Bookmark findByBookmarkIdAndUserId(Long bookmarkId, Long userId);
-    void deleteByBookmarkIdAndUserId(Long bookmarkId, Long userId);
+    void deleteByBookmarkId(Long bookmarkId);
 }
