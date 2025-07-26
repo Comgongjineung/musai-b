ea56d604def61600e5b6c57c12b2d388e9d06c82
mhyeon-kim <0214kkkk@naver.com>
2025-07-26 22:50:02 +0900

    ✨Feat: Add 사용자 관련 API 기능 추가
    사용자와 관련된 기능 API 기능 추가
    1. 회원가입시, 유저 세팅 컬럼 생성
    2. 사용자 정보 수정시, 닉네임 중복 확인
    3. 닉네임 중복 확인 API 추가
    
    Resolves #42

--- src/main/java/com/musai/musai/repository/user/UserRepository.java
+++ src/main/java/com/musai/musai/repository/user/UserRepository.java
@@ -11,4 +11,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByOauthId(String oauthId);
     Optional<User> findByEmail(String email);
     Optional<User> findByOauthProviderAndOauthId(String oauthProvider, String oauthId);
+
+    Optional<User> findByNickname(String nickname);
+    Optional<User> findByNicknameAndUserIdNot(String nickname, Long userId);
+    boolean existsByNickname(String nickname);
+    boolean existsByNicknameAndUserIdNot(String nickname, Long userId);
 }
