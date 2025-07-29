12d787e30281a001a9a9558f7bac4940fd6d819d
RYU <rjw5256@gmail.com>
2025-07-29 19:51:30 +0900

    ✨ Feat: Add 작품 난이도 해설 기능 추가
    작품 난이도 해설 기능만 분리해서 다시 작성하였습니다.
    
    Resolved #34

--- src/main/java/com/musai/musai/dto/difficulty/DifficultyResponseDTO.java
+++ src/main/java/com/musai/musai/dto/difficulty/DifficultyResponseDTO.java
@@ -1,5 +1,6 @@
 package com.musai.musai.dto.difficulty;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DifficultyResponseDTO {
+
+    @JsonProperty("converted")  // AI 서버 JSON 필드명과 매핑
     private String convertedText;
 }
