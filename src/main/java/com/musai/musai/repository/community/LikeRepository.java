01148dbb2e99cfbeb51bd4684c59e572509be717
mhyeon-kim <0214kkkk@naver.com>
2025-08-05 18:07:25 +0900

    ✨Feat: Add 공감 취소 API 추가 및 스웨거 작업
    1. 공감 취소 API 추가
    2. 스웨거 API 설명 추가
    
    Resolves #51

--- src/main/java/com/musai/musai/repository/community/LikeRepository.java
+++ src/main/java/com/musai/musai/repository/community/LikeRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByPostIdAndUserId(Long postId, Long userId);
     List<Like> findAllByUserId(Long userId);
+    Optional<Like> findByPostIdAndUserId(Long postId, Long userId);
+    void deleteByPostIdAndUserId(Long postId, Long userId);
 }
