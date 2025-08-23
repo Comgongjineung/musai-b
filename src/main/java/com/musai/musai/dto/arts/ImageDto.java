8eee954cbe4a85fbc7497d168ce697a44d23349b
RYU <rjw5256@gmail.com>
2025-08-24 01:16:02 +0900

    ✨Feat: Add 선호도 기반 추천 + 더미데이터
    더미데이터를 넣은 선호도 기반 추천 알고리즘 완성했습니다
    
    Related to #93

---
+++ src/main/java/com/musai/musai/dto/arts/ImageDto.java
@@ -0,0 +1,13 @@
+package com.musai.musai.dto.arts;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImageDto {
+    private String primaryImageSmall;
+    private String name;
+    private String title;
+    private String style;
+}
